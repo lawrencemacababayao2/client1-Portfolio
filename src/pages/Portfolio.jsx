5e10f2378af46ec8ae63de28487e241d707c6551
lawrencemacababayao2 <lawrencemacababayao2@gmail.com>
2025-10-26 04:13:23 +0800

    portfolio

--- src/pages/Portfolio.jsx
+++ src/pages/Portfolio.jsx
@@ -330,8 +330,8 @@ const Portfolio = () => {
         <h1 className="text-4xl font-bold text-[#D4AF37]">My Achievements</h1>
       </section>
 
-      <section ref={contactRef} className="bg-[#0F0F0F] w-full h-fit flex-col items-center pt-30 grid grid-cols-1 md:grid-cols-2 p-70 gap-30 text-white/90 pb-10">
-        <div className="w-full h-120">
+      <section ref={contactRef} className="bg-[#0F0F0F] w-full h-fit flex-col items-center pt-30 grid grid-cols-1 md:grid-cols-2 p-10 md:p-70 gap-30 text-white/90 pb-10">
+        <div className="w-full h-120 ">
           <h1 className="text-5xl font-bold text-[#D4AF37] mt-10">Let's Connect</h1>
           <p className="pt-10">Feel free to reach out for collaborations, projects, or inquiries.</p>
           
